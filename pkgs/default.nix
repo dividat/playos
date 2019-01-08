f49a123c1d30501e1a1800d2d400e57b73d05313
pukkamustard <pukkamustard@posteo.net>
2019-01-08 18:43:55 +0100

    Remove dt-utils

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,10 +1,6 @@
 # overlay for custom packages
 self: super: {
 
-  dt-utils = (import ../pkgs/dt-utils) {
-    inherit (super) stdenv fetchgit autoreconfHook libudev pkgconfig;
-  };
-
   rauc = (import ./rauc) super;
 
   dividat-driver = (import ./dividat-driver) {
