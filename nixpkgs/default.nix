5e4a5dc8a7255b2cc3d3979b25bf5fd36528f068
pukkamustard <pukkamustard@posteo.net>
2019-01-08 17:05:53 +0100

    NetworkManager is a royal pain in the ass

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -3,13 +3,13 @@ let
   # Bootstrap with currently available version of nixpkgs
   _nixpkgs = import <nixpkgs> {};
 
-  nixpkgsRepo = _nixpkgs.fetchFromGitHub { 
+  nixpkgsRepo = _nixpkgs.fetchFromGitHub {
     owner = "NixOS"
     ; repo = "nixpkgs-channels"
     ; rev = "2dc559868c94a6aad7cacbebb0ba7abdd9e08d91"
     ; sha256 = "0hass2jk4jaijnsgvfk59kq5rli56bc7xv8gwsc9y6cff5sxfv8g";
   };
 
-in 
+in
   { nixpkgs = import nixpkgsRepo
   ; importFromNixos = path: import (nixpkgsRepo + "/nixos/" + path);}
--- system/configuration.nix
+++ system/configuration.nix
@@ -76,7 +76,22 @@ with lib;
   # Enable non-free firmware
   hardware.enableRedistributableFirmware = true;
 
-  networking.networkmanager.enable = true;
   networking.hostName = "playos";
+
+  # Use NetworkManager
+  networking.networkmanager.enable = true;
+
+  # Use dhcpcd which supports fallback to link-local addressing.
+  # NetworkManager per default does not fall back to link-local addressing (IPv4LL) [1].
+  # [1] https://mail.gnome.org/archives/networkmanager-list/2009-April/msg00073.html
+  networking.networkmanager.dhcp = "dhcpcd";
+
+  # TODO: remove debug
+  environment.etc."dhcpcd.conf".text = ''
+    debug
+  '';
+
+  # Use Google Public DNS as fallback
+  networking.networkmanager.appendNameservers = [ "8.8.8.8" "8.8.4.4" ];
 
 }
