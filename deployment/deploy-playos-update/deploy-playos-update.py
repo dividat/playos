25a8adb771e1aac7efad9dfd3972c17047b6495c
pukkamustard <pukkamustard@posteo.net>
2019-01-21 09:24:13 +0100

    Create release directory from deploy script

--- deployment/deploy-playos-update/deploy-playos-update.py
+++ deployment/deploy-playos-update/deploy-playos-update.py
@@ -2,6 +2,7 @@
 
 import argparse
 import subprocess
+import os.path
 
 KEYRING = "@keyring@"
 UNSIGNED_RAUC_BUNDLE = "@unsignedRaucBundle@"
@@ -32,11 +33,31 @@ def _query_continue(question, default=False):
             sys.stdout.write("Please respond with 'yes' or 'no'")
 
 
-def _main(opts):
+def sign_rauc_bundle(key, cert, keyring, out):
     subprocess.run([
-        RAUC, "--key", opts.key, "--cert", opts.cert, "--keyring", KEYRING,
-        "resign", UNSIGNED_RAUC_BUNDLE, "playos-" + VERSION + ".raucb"
+        RAUC, "--key", key, "--cert", cert, "--keyring", keyring, "resign",
+        UNSIGNED_RAUC_BUNDLE, out
     ])
+
+
+def _main(opts):
+    # Create the output directory
+    os.makedirs(opts.out, exist_ok=True)
+
+    # Create the version directory
+    os.makedirs(os.path.join(opts.out, VERSION), exist_ok=True)
+
+    # Sign RAUC bundle
+    sign_rauc_bundle(
+        key=opts.key,
+        cert=opts.cert,
+        keyring=KEYRING,
+        out=os.path.join(opts.out, VERSION, "playos-" + VERSION + ".raucb"))
+
+    # Write latest file
+    with open(os.path.join(opts.out, 'latest'), 'w') as latest:
+        latest.write(VERSION + "\n")
+
     exit(0)
 
 
@@ -44,6 +65,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Deploy PlayOS update")
     parser.add_argument('-v', '--version', action='version', version=VERSION)
     parser.add_argument('--key', help="key file or PKCS#11 URL", required=True)
+    parser.add_argument(
+        '--out',
+        help="directory to output release files to",
+        default='./release')
     parser.add_argument(
         '--cert', help="cert file or PKCS#11 URL", default=KEYRING)
     _main(parser.parse_args())
