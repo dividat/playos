ea54a401304cdf9ccd6c40140cdaa874bce14b76
Johannes Emerich <johannes@emerich.de>
2019-02-21 17:31:11 +0100

    Add info about time and zone in controller

---
+++ controller/bindings/timedate/timedate.mli
@@ -0,0 +1,30 @@
+type t = OBus_peer.Private.t
+
+(** [daemon ()] returns the peer object for the timedate1 service/daemon *)
+val daemon : unit -> t Lwt.t
+
+(** [get_available_timezones daemon] returns the available timezones in the system *)
+val get_available_timezones : unit -> (string list) Lwt.t
+
+(** [get_current_time daemon] returns the current formatted timestamp *)
+val get_current_time : unit -> string Lwt.t
+
+(** [get_timezone daemon] returns the currently set timezone *)
+val get_timezone : t -> (string option) Lwt.t
+
+(** [set_timezone daemon timezone] sets the timezone *)
+val set_timezone : t -> string -> unit Lwt.t
+
+module Org_freedesktop_timedate1 : sig
+  val timezone : OBus_proxy.t -> (string, [ `readable ]) OBus_property.t
+  val local_rtc : OBus_proxy.t -> (bool, [ `readable ]) OBus_property.t
+  val can_ntp : OBus_proxy.t -> (bool, [ `readable ]) OBus_property.t
+  val ntp : OBus_proxy.t -> (bool, [ `readable ]) OBus_property.t
+  val ntpsynchronized : OBus_proxy.t -> (bool, [ `readable ]) OBus_property.t
+  val time_usec : OBus_proxy.t -> (int64, [ `readable ]) OBus_property.t
+  val rtctime_usec : OBus_proxy.t -> (int64, [ `readable ]) OBus_property.t
+  val set_time : OBus_proxy.t -> int64 -> bool -> bool -> unit Lwt.t
+  val set_timezone : OBus_proxy.t -> string -> bool -> unit Lwt.t
+  val set_local_rtc : OBus_proxy.t -> bool -> bool -> bool -> unit Lwt.t
+  val set_ntp : OBus_proxy.t -> bool -> bool -> unit Lwt.t
+end
