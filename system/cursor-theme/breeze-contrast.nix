457e252e0eca343dd24082abeb20ccc63b43bd53
Joris <joris@guyonvarch.me>
2019-09-24 16:14:01 +0200

    Remove unecessary commands in breeze-contrast derivation

--- system/cursor-theme/breeze-contrast.nix
+++ system/cursor-theme/breeze-contrast.nix
@@ -7,13 +7,11 @@ stdenv.mkDerivation rec {
   src = fetchzip {
     url = "https://code.jpope.org/jpope/breeze_cursor_sources/raw/master/${name}.zip";
     sha256 = "1l8ils82bq2hlsl8shkcirxfjgk0459hsf6zvjnk9zrav47y9vjk";
-    extraPostFetch = "chmod go-w $out";
   };
 
   installPhase = ''
     install -d $out/share/icons/Breeze_Contrast
     cp -rf * $out/share/icons/Breeze_Contrast
-    find $out -type d -exec chmod 555 {} \;
   '';
 
   meta = {
