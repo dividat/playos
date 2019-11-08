734f4bef35fc49acf8bdd3510b46c787e6e3ad57
Joris <joris@guyonvarch.me>
2019-11-08 14:33:37 +0100

    Clean connection state after a connection failure
    Previously, when an error happened during a connection, the agent was
    not cleaned up. At the next connection attempt, an Already Existed error
    was returned. A reboot of PlayOS was required to be able to connect to a
    wifi network.
    
    The proposed change clean up the agent, no matters if the connection
    succeeds or fail. It allows to reconnect without restarting PlayOS.

--- controller/bindings/connman/connman.ml
+++ controller/bindings/connman/connman.ml
@@ -95,7 +95,7 @@ struct
     match input with
     | Passphrase p ->
       (match List.assoc_opt "Passphrase" fields with
-       | Some _ -> 
+       | Some _ ->
          return [ "Passphrase", p |> OBus_value.C.(make_single basic_string)]
        | None ->
          let%lwt () = Logs_lwt.err ~src:log_src
@@ -340,20 +340,21 @@ struct
     let%lwt () = Logs_lwt.debug ~src:log_src
         (fun m -> m "connect to service %s" service.id)
     in
+
     (* Create and register an agent that will pass input to ConnMan *)
     let%lwt agent_path, agent = Agent.create input in
     let%lwt () = register_agent service._manager agent_path in
 
-    (* Connect to service *)
-    let%lwt () =
+    begin
+      (* Connect to service *)
       OBus_method.call
         Connman_interfaces.Net_connman_Service.m_Connect
         service._proxy ()
-    in
-
-    (* Cleanup and destroy agent *)
-    let%lwt () = unregister_agent service._manager agent_path in
-    Agent.destroy agent
+    end
+    [%lwt.finally
+      (* Cleanup and destroy agent *)
+      let%lwt () = unregister_agent service._manager agent_path in
+      Agent.destroy agent]
 
   let disconnect service =
     let%lwt () = Logs_lwt.debug ~src:log_src
