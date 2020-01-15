d7d521f8b349dcfc8b4dd3484aad36d4eb0c6106
Joris <joris@guyonvarch.me>
2020-01-15 16:49:10 +0100

    Use common nixpkgs from controller
    PlayOS is currently using the 19.03 version of nixpkgs. It contains
    everything that was previously required for ocaml.

--- controller/default.nix
+++ controller/default.nix
@@ -1,16 +1,6 @@
-{ fetchFromGitHub ? (import <nixpkgs> {}).fetchFromGitHub
-, version ? "0.0.0"
-, updateUrl ? "http://localhost:9999/"
-, kioskUrl ? "https://dev-play.dividat.com/"}:
+{ pkgs, version, updateUrl, kioskUrl }:
 
-# We require two things for the OCaml build environment that are not yet in 18.09 channel (used by rest of project): https://github.com/NixOS/nixpkgs/pull/49684 and https://github.com/NixOS/nixpkgs/pull/53357.
-with import (fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "43d4f07bf1144d6eb20975c956c7cc7bc643ea6e";
-    sha256 = "0gm3majn0gvkdq2zvqrwyn1bi2zd8g0w34rngpfhdcf767l3l2l3";
-    }) {overlays = [ (import ./nix/overlay.nix) ];};
-
+with pkgs;
 
 ocamlPackages.buildDunePackage rec {
   pname = "playos_controller";
