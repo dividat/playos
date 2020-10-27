73ee991a5f993ea5d4642d0a84bddf0cb4193970
Joris <joris@guyonvarch.me>
2020-10-27 16:02:28 +0100

    Add documentation to pacproxy.py

--- system/networking/pacproxy.py
+++ system/networking/pacproxy.py
@@ -1,5 +1,16 @@
 import dbus
 
+"""Ask pacrunner for the proxy to use with https://dividat.com.
+
+Pacrunner can currently only be configured with a fixed proxy URL through the
+controller. We are not using its PAC capabilities.
+
+So, we don’t need to query pacrunner before each request, because the proxy is
+fixed, it does not depend on individual HTTP requests.
+
+We’re using https://dividat.com, but we could have used any valid URL.
+"""
+
 bus = dbus.SystemBus()
 
 client = dbus.Interface(
