f9ee424c59bba33058ee82ab1a8631f298a7e18c
Joris <joris@guyonvarch.me>
2021-04-08 13:11:45 +0200

    Remove pcsclite pin on 1.8.23
    This upgrade pcsclite to version 1.9.0, which is the version in our
    nixpkgs 20.09 version.
    
    Now, why it was required (thank’s Johannes):
    
    - pcsclited runs as a daemon (server) that can talk to smart cards.
    
    - pcsclite also provides a library that allows user-space programs to
    talk to pcsclited; this library is usually dynamically linked.
    
    - We statically link the pcsclite library in the driver, the idea was
    that this makes the driver more standalone and portable.
    
    - pcsclite changed the ABI between pcsclite library and server in an
    incompatible way, which is not a problem when the library is dynamically
    linked, since usually library and server change together on a system. It
    is a problem when the pcsclite library is statically linked and the
    daemon is a different, incompatible version.
    
    This means the pcsclited daemon and library used by the driver need to
    be compatible versions, or need to be dynamically linked.
    
    When first including the driver in PlayOS, we referenced the released
    (statically built) binary and pinned pcsclite to a compatible version.
    If we now actually build the driver as part of PlayOS, we don't have to
    do a static build and would then no longer need to pin pcsclite.
    
    There is another card for figuring out how to proceed with the driver
    releases generally:
    https://trello.com/c/8tD8pDpu/899-future-release-model-for-driver

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -35,15 +35,6 @@ let
         system_version = version;
       };
 
-      # pin pcsclite to 1.8.23 because of break in ABI (https://github.com/LudovicRousseau/PCSC/commit/984f84df10e2d0f432039e3b31f94c74e95092eb)
-      pcsclite = super.pcsclite.overrideAttrs (oldAttrs: rec {
-        version = "1.8.23";
-        src = super.fetchurl {
-          url = "https://pcsclite.apdu.fr/files/pcsc-lite-${version}.tar.bz2";
-          sha256 = "1jc9ws5ra6v3plwraqixin0w0wfxj64drahrbkyrrwzghqjjc9ss";
-        };
-      });
-
       breeze-contrast-cursor-theme = super.callPackage ./breeze-contrast-cursor-theme {};
 
       ocamlPackages = super.ocamlPackages.overrideScope' (self: super: {
