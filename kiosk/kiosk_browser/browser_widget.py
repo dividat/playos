daa1ecace8c06e9adedaa6c7881629c5d5163eb3
Joris <joris@guyonvarch.me>
2022-04-06 15:13:55 +0200

    Introduce loading and network error states in browser widget
    When there is no network, a white page or the default network error page
    with a dinosaur was shown. Which was not very informative.
    
    Instead, store the loading / error state and be in control of what we
    show.
    
    See https://trello.com/c/fy756Bl7/917-handle-lack-of-internet-in-playos-ui

--- kiosk/kiosk_browser/browser_widget.py
+++ kiosk/kiosk_browser/browser_widget.py
@@ -1,61 +1,90 @@
-import re
-import urllib
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
+from enum import Enum, auto
 import logging
-from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QShortcut
-from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
-from PyQt5.QtWidgets import QSizePolicy
+import re
 
 from kiosk_browser import system
 
-class BrowserWidget(QWebEngineView):
+"""
+Webview loading status
+"""
+class Status(Enum):
+    INITIAL_LOADING = auto()
+    NETWORK_ERROR = auto()
+    LOADED = auto()
 
-    def __init__(self, url, get_current_proxy, *args, **kwargs):
-        QWebEngineView.__init__(self, *args, **kwargs)
+class BrowserWidget(QtWidgets.QWidget):
+
+    def __init__(self, url, get_current_proxy, parent):
+        QtWidgets.QWidget.__init__(self, parent)
+
+        self._url = url
+
+        self._layout = QtWidgets.QHBoxLayout()
+        self._layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self._layout)
+
+        # Init views
+        self._loading_page = loading_page(self)
+        self._network_error_page = network_error_page(self)
+        self._webview = QtWebEngineWidgets.QWebEngineView(self)
+
+        # Add views to layout
+        self._layout.addWidget(self._loading_page)
+        self._layout.addWidget(self._network_error_page)
+        self._layout.addWidget(self._webview)
 
         # Register proxy authentication handler
-        self.page().proxyAuthenticationRequired.connect(
+        self._webview.page().proxyAuthenticationRequired.connect(
             lambda url, auth, proxyHost: self._proxy_auth(
                 get_current_proxy, url, auth, proxyHost))
 
         # Override user agent
-        self.page().profile().setHttpUserAgent(user_agent_with_system(
-            user_agent = self.page().profile().httpUserAgent(),
+        self._webview.page().profile().setHttpUserAgent(user_agent_with_system(
+            user_agent = self._webview.page().profile().httpUserAgent(),
             system_name = system.NAME,
             system_version = system.VERSION
         ))
 
         # Allow sound playback without user gesture
-        self.page().settings().setAttribute(QWebEngineSettings.PlaybackRequiresUserGesture, False)
+        self._webview.page().settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PlaybackRequiresUserGesture, False)
 
         # Load url
-        self.page().setUrl(url)
+        self._webview.setUrl(url)
+        self._view(Status.INITIAL_LOADING)
+        self._webview.loadFinished.connect(self._load_finished)
 
-        # Shortcut to manually reload
-        self.reload_shortcut = QShortcut('CTRL+R', self)
-        self.reload_shortcut.activated.connect(self.reload)
-
-        # Check if pages is correctly loaded
-        self.loadFinished.connect(self._load_finished)
-
-        # Shortcut to close
-        self.quit_shortcut = QShortcut('CTRL+ALT+DELETE', self)
-        self.quit_shortcut.activated.connect(lambda: self.close())
-
-        # Stretch the browser
-        policy = QSizePolicy()
+        # Stretch the view
+        policy = QtWidgets.QSizePolicy()
         policy.setVerticalStretch(1)
         policy.setHorizontalStretch(1)
-        policy.setVerticalPolicy(QSizePolicy.Preferred)
-        policy.setHorizontalPolicy(QSizePolicy.Preferred)
+        policy.setVerticalPolicy(QtWidgets.QSizePolicy.Preferred)
+        policy.setHorizontalPolicy(QtWidgets.QSizePolicy.Preferred)
         self.setSizePolicy(policy)
 
-    def load(self, url):
-        self.page().setUrl(url)
+        # Shortcut to manually reload
+        self.reload_shortcut = QtWidgets.QShortcut('CTRL+R', self)
+        self.reload_shortcut.activated.connect(self.reload)
+
+    def show_blank_page(self):
+        """ Hide browser widget by showing a blank page.
+        """
+        self._webview.setHtml("")
+
+    def reload(self):
+        """ Show kiosk browser loading URL.
+        """
+        self._webview.setUrl(self._url)
+        self._view(Status.INITIAL_LOADING)
+
+    # Private
 
     def _load_finished(self, success):
+        if success:
+            self._view(Status.LOADED)
         if not success:
-            QTimer.singleShot(5000, self.reload)
+            self._view(Status.NETWORK_ERROR)
+            QtCore.QTimer.singleShot(5000, self._webview.reload)
 
     def _proxy_auth(self, get_current_proxy, url, auth, proxyHost):
         proxy = get_current_proxy()
@@ -65,6 +94,20 @@ class BrowserWidget(QWebEngineView):
             auth.setPassword(proxy.password)
         else:
             logging.info("Proxy authentication request ignored because credentials are not provided.")
+
+    def _view(self, status):
+        if status == Status.INITIAL_LOADING:
+            self._loading_page.show()
+            self._network_error_page.hide()
+            self._webview.hide()
+        elif status == Status.NETWORK_ERROR:
+            self._loading_page.hide()
+            self._network_error_page.show()
+            self._webview.hide()
+        elif status == Status.LOADED:
+            self._loading_page.hide()
+            self._network_error_page.hide()
+            self._webview.show()
 
 def user_agent_with_system(user_agent, system_name, system_version):
     """Inject a specific system into a user agent string"""
@@ -80,3 +123,17 @@ def user_agent_with_system(user_agent, system_name, system_version):
             system_detail = f"{m.group(2)}; {system_name} {system_version}"
 
         return f"{m.group(1)} ({system_detail}){m.group(3)}"
+
+def loading_page(parent):
+    """ Show a loader in the middle of a blank page.
+    """
+
+    widget = QtWidgets.QLabel("Loading…", parent)
+    return widget
+
+def network_error_page(parent):
+    """ Show network error message.
+    """
+
+    widget = QtWidgets.QLabel("Network error, sorry…", parent)
+    return widget
