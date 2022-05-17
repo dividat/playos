6be509a95b1a57f34c1bf518e42fa3bfb5e97965
Gábor Kerekes <krks.gbr@gmail.com>
2022-05-17 21:14:32 +0200

    Refactor screen resolution settings
    - Factor out reading and writing screen scaling settings to a separate module.
    
    - Make it possible to change default scaling behaviour between releases.
      This is ensured by introducing a third setting: "default".
      With the "default" setting there is no configuration file created.
      The absence of the configuration file can be interpreted in the
      xsession init script in a flexible way, allowing us to change the
      default behaviour between releases. Right now "default" means we don't
      scale to Full HD, but we can easily change this by editing the
      xsession init script in `play-kiosk.nix`.

---
+++ controller/bindings/screen-settings/screen_settings.ml
@@ -0,0 +1,61 @@
+open Lwt
+
+let log_src = Logs.Src.create "screen-scaling"
+let settings_file = "/var/lib/gui-localization/screen-scaling"
+
+(* Scaling options *)
+type scaling =
+  (* No config file
+     xsession script decides behaviour
+  *)
+  | Default
+  (* Config file with keyword: "scaled"
+     Explicitly opt-in to scaling to FullHD
+  *)
+  | Scaled
+  (* Config file with keyword: "native"
+     Explicitly opt-out of scaling to FullHD
+  *)
+  | Native
+
+let scaling_of_string = function
+  | "default" -> Some Default
+  | "scaled" -> Some Scaled
+  | "native" -> Some Native
+  | _ -> None
+
+let string_of_scaling = function
+  | Default -> "default"
+  | Scaled -> "scaled"
+  | Native -> "native"
+
+(* Used for representing options in the UI, ie. in dropdown. *)
+let label_of_scaling = function
+  | Default -> "Default"
+  | Scaled -> "Full HD"
+  | Native -> "Native"
+
+let set_scaling scaling =
+  match scaling with
+    | Default ->
+       Lwt_unix.file_exists settings_file
+         >>= (fun exists ->
+                if exists then Lwt_unix.unlink settings_file
+                else  return ()
+             )
+    | _ ->
+       Util.write_to_file log_src settings_file (string_of_scaling scaling)
+
+let get_scaling () =
+  Lwt_unix.file_exists settings_file
+    >>= (fun exists ->
+            if exists then
+               Util.read_from_file log_src settings_file
+                 >|= (fun s ->
+                        s
+                          |> scaling_of_string
+                          |> Option.value ~default:Default
+                     )
+             else
+               return Default
+        )
