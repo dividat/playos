6480fc5221430911297b16ba9ab4523635ba81d7
Johannes Emerich <johannes@dividat.com>
2022-11-09 07:26:18 +0100

    Use fetchTarball to pin nixpkgs
    `fetchGit` fetches more than we need and thereby wastes space and
    bandwidth, especially with a major repository like Nixpkgs.
    
    Also getting rid of the patching mechanism as we do not currently need
    it. Technique moved to long term memory here:
    https://bits.dividat.com/2022/patching-nixpkgs-pins/

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,14 +2,10 @@
 
 let
 
-  nixpkgs = import ./patch-nixpkgs.nix {
-    src = builtins.fetchGit {
-      name = "nixos-21.11-2021-03-07";
-      url = "https://github.com/nixos/nixpkgs";
-      ref = "refs/heads/nixos-21.11";
-      rev = "9b1c7ba323732ddc85a51850a7f10ecc5269b8e9";
-    };
-    patches = [];
+  nixpkgs = builtins.fetchTarball {
+    # nixos-21.11 2022-03-07
+    url = "https://github.com/nixos/nixpkgs/archive/9b1c7ba323732ddc85a51850a7f10ecc5269b8e9.tar.gz";
+    sha256 = "12m4bkbqdnwxq607w58fqnmx8wnii2f6g2rjlb4wwp79apkrzwb6";
   };
 
   overlay =
