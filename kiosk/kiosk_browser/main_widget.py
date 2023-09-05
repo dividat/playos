8fb49c546ed9bbe10d2f98f4c178904a1decd5d6
Joris <joris@guyonvarch.me>
2023-09-05 18:17:24 +0200

    Give access to captive portal when on settings
    Previously, when the settings modal was open, when a captive portal was
    detected, we showed a header with a button, but it could not be
    interacted with, because the modal didn’t allow to access the parent
    window.
    
    Setup the settings modal to be modeless, so that the button to open the
    captive portal is still accessible.
    
    Also better represent the dialog state with an enumeration.
    
    See https://trello.com/c/wcIrvEau/1445-captive-portal-prompt-pops-up-beneath-controller-modal

--- kiosk/kiosk_browser/main_widget.py
+++ kiosk/kiosk_browser/main_widget.py
@@ -1,8 +1,17 @@
 from PyQt5 import QtWidgets, QtCore
+from enum import Enum, auto
 
 from kiosk_browser import browser_widget, captive_portal
 from kiosk_browser import proxy as proxy_module
 from kiosk_browser import webview_dialog
+
+"""
+Dialog View being open
+"""
+class DialogView(Enum):
+    CLOSED = auto()
+    SETTINGS = auto()
+    CAPTIVE_PORTAL = auto()
 
 class MainWidget(QtWidgets.QWidget):
 
@@ -27,14 +36,16 @@ class MainWidget(QtWidgets.QWidget):
         self._layout.setSpacing(0)
         self._layout.addWidget(self._browser_widget)
 
+        # Dialog
+        self._dialog_view = DialogView.CLOSED
+
         # Captive portal
-        self._is_captive_portal_dialog_open = False
         self._captive_portal_url = ''
         self._captive_portal_message = captive_portal.open_message(self._show_captive_portal)
         self._captive_portal = captive_portal.CaptivePortal(proxy.get_current, self._show_captive_portal_message)
         self._captive_portal.start_monitoring_daemon()
 
-        QtWidgets.QShortcut(toggle_settings_key, self).activated.connect(self._show_settings)
+        QtWidgets.QShortcut(toggle_settings_key, self).activated.connect(self._toggle_settings)
 
         self.setLayout(self._layout)
         self.show()
@@ -43,31 +54,46 @@ class MainWidget(QtWidgets.QWidget):
 
     def _show_captive_portal_message(self, url):
         self._captive_portal_url = QtCore.QUrl(url)
-        if self._captive_portal_message.parentWidget() == None and not self._is_captive_portal_dialog_open:
+        if self._captive_portal_message.parentWidget() == None and not self._dialog_view == DialogView.CAPTIVE_PORTAL:
             self._layout.addWidget(self._captive_portal_message)
+
+    def _toggle_settings(self):
+        if self._dialog_view == DialogView.CLOSED:
+            self._show_settings()
+        else:
+            self._on_dialog_close()
 
     def _show_settings(self):
         self._browser_widget.show_overlay()
-        webview_dialog.widget(
+        self._dialog = webview_dialog.widget(
                 parent = self, 
                 title = "System Settings", 
                 url = self._settings_url, 
                 additional_close_keys = [self._toggle_settings_key],
-                on_close = self._browser_widget.reload
-            ).exec_()
+                on_close = self._on_dialog_close
+            )
+        # Open modeless to allow accessing captive portal message banner
+        # https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QDialog.html#modeless-dialogs
+        self._dialog.show()
+        self._dialog_view = DialogView.SETTINGS
 
     def _show_captive_portal(self):
+        if not self._dialog_view == DialogView.CLOSED:
+            self._dialog.close()
         self._browser_widget.show_overlay()
         self._is_captive_portal_dialog_open = True
         self._captive_portal_message.setParent(None)
-        webview_dialog.widget(
+        self._dialog = webview_dialog.widget(
                 parent = self, 
                 title = "Network Login", 
                 url = self._captive_portal_url,
-                additional_close_keys = [],
-                on_close = self._on_captive_portal_dialog_close
-            ).exec_()
+                additional_close_keys = [self._toggle_settings_key],
+                on_close = self._on_dialog_close
+            )
+        self._dialog.show()
+        self._dialog_view = DialogView.CAPTIVE_PORTAL
 
-    def _on_captive_portal_dialog_close(self):
-        self._is_captive_portal_dialog_open = False
+    def _on_dialog_close(self):
+        self._dialog_view = DialogView.CLOSED
         self._browser_widget.reload()
+        self._dialog.close()
