1c9058ef6cc3dc623f9b5ff08113f920a7a320c9
Johannes Emerich <johannes@dividat.com>
2024-05-06 15:45:30 +0200

    Use longform flags for iptables
    Co-authored-by: Joris Guyonvarch <joris@guyonvarch.me>

--- application.nix
+++ application.nix
@@ -125,7 +125,7 @@ rec {
         # Allow use of TFTP client for Senso firmware update
         connectionTrackingModules = [ "tftp" ];
         extraCommands = ''
-          iptables -t raw -A OUTPUT -p udp --dport 69 -j CT --helper tftp
+          iptables --table raw --append OUTPUT --protocol udp --dport 69 --jump CT --helper tftp
         '';
       };
 
