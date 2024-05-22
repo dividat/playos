33d1228e26024923c071010e67c23288e4788302
Gábor Kerekes <krks.gbr@gmail.com>
2024-05-22 12:36:50 +0200

    Update driver to 2.4.0

--- application/overlays/dividat-driver/default.nix
+++ application/overlays/dividat-driver/default.nix
@@ -2,7 +2,7 @@
 
 let
 
-  version = "2.3.0-105-g36e3e91";
+  version = "2.4.0";
 
 in buildGoModule rec {
 
@@ -12,8 +12,8 @@ in buildGoModule rec {
   src = fetchFromGitHub {
     owner = "dividat";
     repo = "driver";
-    rev = "36e3e91529c27e47fde9033f44154e22f2960b40";
-    sha256 = "sha256-uYCPBllDYCynPvekP6siop5u6uXY/0Kd4enL2/0nmcU=";
+    rev = version;
+    sha256 = "sha256-JeyV01WAhuPw1BIG1EJ4ET5HjSc7VwoWTZ/6EvVEC5Q=";
   };
 
   vendorHash = "sha256-Jj6aI85hZXGeWhJ8wq9MgI9uTm11tJZUdVwI90Pio4s=";
