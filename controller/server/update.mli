e65397c7c8f4c98e9aaf4f77a2c50adaf3b9f923
Ignas Vyšniauskas <i.vysniauskas@gmail.com>
2024-08-04 21:07:23 +0300

    Refactor update module to enable testing
    Refactors the Update module's interface into a functor over its
    dependencies (RAUC, Connman/Curl and runtime configuration). This
    enables passing mock implementations of the dependencies for testing.

--- controller/server/update.mli
+++ controller/server/update.mli
@@ -26,4 +26,14 @@ type state =
   | ReinstallRequired
 [@@deriving sexp]
 
+module UpdateService : functor (_ : Update_deps.UpdateServiceDeps) -> sig
+  val run : set_state:(state -> unit) -> state -> unit Lwt.t
+
+  (* private functions used in testing *)
+  module Private : sig
+    val run_step : state -> state Lwt.t
+  end
+end
+
+(* maintaining the original entrypoint for backwards compatibility *)
 val start : connman:Connman.Manager.t -> rauc:Rauc.t -> update_url:string -> state Lwt_react.signal * unit Lwt.t
