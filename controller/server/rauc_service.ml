6fbd25a90ce63ab1b693248cf1392d1972cd824f
Ignas Vyšniauskas <i.vysniauskas@gmail.com>
2024-08-26 09:03:07 +0300

    Remove obsolete rauc reference in RaucServiceIntf docs

--- controller/server/rauc_service.ml
+++ controller/server/rauc_service.ml
@@ -1,17 +1,17 @@
 module type RaucServiceIntf = sig
-    (** [get_status rauc] returns current RAUC status *)
+    (** [get_status unit] returns current RAUC status *)
     val get_status : unit -> Rauc.status Lwt.t
 
-    (** [get_booted_slot rauc] returns the currently booted slot *)
+    (** [get_booted_slot unit] returns the currently booted slot *)
     val get_booted_slot : unit -> Rauc.Slot.t Lwt.t
 
-    (** [mark_good rauc slot] marks the slot [slot] as good *)
+    (** [mark_good slot] marks the slot [slot] as good *)
     val mark_good : Rauc.Slot.t -> unit Lwt.t
 
-    (** [get_primary rauc] returns current primary slot, if any *)
+    (** [get_primary unit] returns current primary slot, if any *)
     val get_primary : unit -> Rauc.Slot.t option Lwt.t
 
-    (** [install rauc source] install the bundle at path [source] *)
+    (** [install source] install the bundle at path [source] *)
     val install : string -> unit Lwt.t
 end
 
