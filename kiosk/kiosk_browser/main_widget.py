4d3b36dff1e5c57f8a7166fa4b5768a8b40b5ada
Joris <joris@guyonvarch.me>
2024-09-24 10:43:51 +0200

    Open settings when long pressing the Menu key

--- kiosk/kiosk_browser/main_widget.py
+++ kiosk/kiosk_browser/main_widget.py
@@ -1,12 +1,13 @@
 from PyQt5 import QtWidgets, QtCore
+import time
 
 from kiosk_browser import browser_widget, captive_portal, dialogable_widget, proxy as proxy_module
 
 class MainWidget(QtWidgets.QWidget):
-    """ Show website at kiosk_url.
+    """ Show website from kiosk_url.
 
-    - Show settings in dialog using shortcut.
-    - Show message when captive portal is detected, allowing to show in dialog.
+    - Show settings in a dialog using a shortcut or long pressing Menu.
+    - Show toolbar message when captive portal is detected, opening it in a dialog.
     - Use proxy configured in Connman.
     """
 
@@ -17,14 +18,18 @@ class MainWidget(QtWidgets.QWidget):
         proxy = proxy_module.Proxy()
         proxy.start_monitoring_daemon()
 
+        # Menu press
+        self._menu_press_since = None
+        self._menu_press_delay_seconds = 1.5
+
         # Browser widget
         self._kiosk_url = kiosk_url
         self._settings_url = settings_url
         self._dialogable_browser = dialogable_widget.DialogableWidget(
             parent = self,
             inner_widget = browser_widget.BrowserWidget(
-                url = kiosk_url, 
-                get_current_proxy = proxy.get_current, 
+                url = kiosk_url,
+                get_current_proxy = proxy.get_current,
                 parent = self),
             on_close = self._close_dialog)
 
@@ -43,8 +48,11 @@ class MainWidget(QtWidgets.QWidget):
         self._layout.addWidget(self._dialogable_browser)
         self.setLayout(self._layout)
 
-        # Shortcuts
+        # Shortcut to toggle settings
         QtWidgets.QShortcut(toggle_settings_key, self).activated.connect(self._toggle_settings)
+
+        # Look at events with the eventFilter function
+        self.installEventFilter(self)
 
     # Private
 
@@ -73,3 +81,18 @@ class MainWidget(QtWidgets.QWidget):
             self._dialogable_browser.inner_widget().load(self._kiosk_url)
             if self._is_captive_portal_open:
                 self._is_captive_portal_open = False
+
+    def eventFilter(self, source, event):
+        # Toggle settings with a long press on the Menu key
+        if event.type() == QtCore.QEvent.ShortcutOverride:
+            if event.key() == QtCore.Qt.Key_Menu:
+                if self._menu_press_since is None:
+                    self._menu_press_since = time.time()
+                elif time.time() - self._menu_press_since > self._menu_press_delay_seconds:
+                    self._menu_press_since = None
+                    self._toggle_settings()
+        elif event.type() == QtCore.QEvent.KeyRelease:
+            if event.key() == QtCore.Qt.Key_Menu:
+                self._menu_press_since = None
+
+        return super(MainWidget, self).eventFilter(source, event)
