608fa1fe21e24cddf24f5f04ab477ff8c0ab50c3
Ignas Vyšniauskas <i.vysniauskas@gmail.com>
2024-10-07 13:57:45 +0300

    Forward test logs to QEMU serial device like 'normal' NixOS tests

--- testing/end-to-end/profile.nix
+++ testing/end-to-end/profile.nix
@@ -1,4 +1,12 @@
-{pkgs, ...}: {
+{pkgs, ...}:
+let
+  qemu-common = pkgs.importFromNixos "lib/qemu-common.nix" {
+    inherit pkgs;
+    inherit (pkgs) lib;
+  };
+in
+{
+
     imports = [
       (pkgs.importFromNixos "modules/profiles/qemu-guest.nix")
       (pkgs.importFromNixos "modules/testing/test-instrumentation.nix")
@@ -7,5 +15,12 @@
     config = {
         # don't need opengl for running tests, reduces image size vastly
         hardware.opengl.enable = false;
+
+        # test-instrumentation.nix sets this in the boot as kernel param,
+        # but since we are booting with a custom GRUB config it has no effect,
+        # so instead we set this directly in journald
+        services.journald.extraConfig = ''
+            TTYPath = ${qemu-common.qemuSerialDevice};
+        '';
     };
 }
