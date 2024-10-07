d6d446387336a91fe409c4b128d44fd4309eb6f8
Johannes Emerich <johannes@dividat.com>
2024-10-07 14:02:38 +0200

    Bump nixpkgs to 24.05 channel

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,9 +3,9 @@
 let
 
   nixpkgs = builtins.fetchTarball {
-    # nixos-23.11 2024-03-18
-    url = "https://github.com/nixos/nixpkgs/archive/614b4613980a522ba49f0d194531beddbb7220d3.tar.gz";
-    sha256 = "1kipdjdjcd1brm5a9lzlhffrgyid0byaqwfnpzlmw3q825z7nj6w";
+    # nixos-24.05 2024-10-07
+    url = "https://github.com/nixos/nixpkgs/archive/ecbc1ca8ffd6aea8372ad16be9ebbb39889e55b6.tar.gz";
+    sha256 = "0yfaybsa30zx4bm900hgn3hz92javlf4d47ahdaxj9fai00ddc1x";
   };
 
   overlay =
