8cf31eece22c2ba621c955f0859ab3015783a30a
Ignas Vyšniauskas <i.vysniauskas@gmail.com>
2024-10-17 23:17:23 +0300

    Re-use factory-reset test scenario in integration and e2e tests

--- testing/integration/factory-reset.nix
+++ testing/integration/factory-reset.nix
@@ -87,24 +87,7 @@ subprocess.run(["${pkgs.e2fsprogs}/bin/mkfs.ext4",
 )
 
 # ==== Test scenario
-
-playos.start(allow_reboot=True)
-
-with TestPrecondition("Persistent data is mounted"):
-    playos.wait_for_unit('mnt-data.mount')
-
-with TestCase("Persistent data remains after reboot"):
-    playos.succeed("echo TEST_DATA > /mnt/data/persist-me")
-    playos.shutdown()
-    playos.start(allow_reboot=True)
-    playos.wait_for_unit('mnt-data.mount')
-    playos.succeed("grep TEST_DATA /mnt/data/persist-me")
-
-with TestCase("Persistent data is wiped if factory reset is triggered"):
-    playos.succeed("systemctl start playos-wipe-persistent-data.service")
-    playos.shutdown()
-    playos.start()
-    playos.wait_for_unit('mnt-data.mount')
-    playos.succeed("test ! -f /mnt/data/persist-me")
-  '';
+# scenario is re-used in e2e tests as well
+${builtins.readFile ./factory-reset-scenario.py}
+'';
 }
