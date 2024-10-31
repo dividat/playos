a5f6b5fd6312aee5f72ef2a40f9413f105cff677
Ignas Vyšniauskas <i.vysniauskas@gmail.com>
2024-10-31 11:07:45 +0200

    Version that works on QEMU

--- application/select-display.sh
+++ application/select-display.sh
@@ -2,34 +2,47 @@ set -euo pipefail
 
 SCALING_PREF=$(cat /var/lib/gui-localization/screen-scaling 2>/dev/null || echo "default")
 readonly SCALING_PREF
-echo "Using scaling preference '$SCALING_PREF'"
 
 CONNECTED_OUTPUTS=$(xrandr | grep ' connected' | awk '{ print $1 }')
 readonly CONNECTED_OUTPUTS
+
 echo -e "Connected outputs:\n$CONNECTED_OUTPUTS\n"
+
+scaling_pref_params=""
+
+echo "Using scaling preference '$SCALING_PREF'"
+
+case "$SCALING_PREF" in
+    "default" | "full-hd")
+        scaling_pref_params=(--mode 1920x1080)
+        ;;
+    "native")
+        scaling_pref_params=(--auto)
+        ;;
+    *)
+        scaling_pref_params=(--auto)
+        ;;
+esac
 
 if [ -z "$CONNECTED_OUTPUTS" ]; then
 
-  echo "No connected outputs found. Apply xrandr globally."
-  xrandr --auto
+    echo "No connected outputs found. Attempting to apply xrandr globally."
+    xrandr "${scaling_pref_params[@]}"
 
 else
 
-  case "$SCALING_PREF" in
-    "default" | "full-hd")
-      for output in $CONNECTED_OUTPUTS; do
-        echo "Applying full-hd to output '$output'"
-        xrandr --auto --output "$output" --mode 1920x1080
-      done
-      ;;
-    "native")
-      echo "Native scaling preference. Applying auto."
-      xrandr --auto
-      ;;
-    *)
-      echo "Unknown scaling preference '$SCALING_PREF'. Applying auto."
-      xrandr --auto
-      ;;
-  esac
+    first_output=""
+    for output in $CONNECTED_OUTPUTS; do
+        if [ -z "$first_output" ]; then
+            first_output=$output
+            xrandr --output "$output" \
+                --primary \
+                "${scaling_pref_params[@]}"
+        else
+            xrandr --output "$output" \
+                --same-as "$first_output" \
+                "${scaling_pref_params[@]}"
+        fi
+    done
 
 fi
