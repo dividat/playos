c8345a5b8c327808df0f4d13264db0dfe9dd6a47
Ignas Vyšniauskas <i.vysniauskas@gmail.com>
2024-11-15 15:29:20 +0200

    Add ppx_protocol_conv and ppx_protocol_conv_jsonm to ocaml-modules

---
+++ pkgs/ocaml-modules/ppx_protocol_conv/default.nix
@@ -0,0 +1,43 @@
+{ buildDunePackage
+, fetchFromGitHub
+, lib
+, base
+, ppxlib
+, ppx_fields_conv
+, ppx_sexp_conv
+, sexplib
+, alcotest
+}:
+
+buildDunePackage rec {
+  pname = "ppx_protocol_conv";
+  version = "5.2.1";
+
+  minimumOCamlVersion = "4.08";
+
+  src = fetchFromGitHub {
+    owner = "andersfugmann";
+    repo = "ppx_protocol_conv";
+    rev = "9a1c4f450cf4fc797e09296d736e5c21f6eb7c4a";
+    sha256 = "sha256-3WlpuuRJfZA6UaeuJRuYI2Z9J7Yng+/J09BDmETwy30=";
+  };
+
+  useDune2 = true;
+
+  #nativeBuildInputs = [ ];
+  propagatedBuildInputs = [
+    base
+    ppxlib
+    ppx_fields_conv
+    ppx_sexp_conv
+    sexplib
+    alcotest
+  ];
+
+  meta = {
+    homepage = https://github.com/andersfugmann/ppx_protocol_conv;
+    description = "Ppx for generating serialisation and de-serialisation functions of ocaml types";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
