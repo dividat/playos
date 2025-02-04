d3799ad9ade07f99df9f67e71c22c9ebc0750d24
Johannes Emerich <johannes@dividat.com>
2025-02-04 15:39:51 +0100

    Extend power button test to regular keyboard being ignored

--- testing/integration/power-button-shutdown.nix
+++ testing/integration/power-button-shutdown.nix
@@ -10,17 +10,33 @@ pkgs.testers.runNixOSTest {
     };
   };
 
+  extraPythonPackages = ps: [
+    ps.colorama
+    ps.types-colorama
+  ];
+
   testScript = {nodes}:
 ''
-machine.start()
-machine.wait_for_unit("multi-user.target")
-machine.wait_for_console_text("Power Button")
+${builtins.readFile ../helpers/nixos-test-script-helpers.py}
+import time
 
-print(machine.succeed("cat /proc/bus/input/devices"))
+with TestPrecondition("Power Button has been recognized"):
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+    machine.wait_for_console_text("Power Button")
 
-# Trigger ACPI shutdown command and expect shutdown
-machine.send_monitor_command("system_powerdown")
-machine.wait_for_console_text("Stopped target Multi-User System")
+    print(machine.succeed("cat /proc/bus/input/devices"))
+
+with TestCase("Short press on power and sleep from regular keyboard are ignored"):
+    # https://github.com/qemu/qemu/blob/master/pc-bios/keymaps/en-us
+    machine.send_monitor_command("sendkey 0xde") # XF86PowerOff
+    machine.send_monitor_command("sendkey 0xdf") # XF86Sleep
+    time.sleep(5)
+    machine.succeed("echo still alive", timeout=1)
+
+with TestCase("ACPI shutdown command invokes shutdown"):
+    machine.send_monitor_command("system_powerdown")
+    machine.wait_for_console_text("Stopped target Multi-User System")
 
 # Test script does not finish on its own after shutdown
 exit(0)
