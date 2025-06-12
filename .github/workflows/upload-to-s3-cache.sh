2fa5ba34800a50e4f71d887d7a193dd380a6104c
Ignas Vyšniauskas <ignas@dividat.ch>
2025-06-12 14:53:12 +0300

    WIP cache reading/writing setup

---
+++ .github/workflows/upload-to-s3-cache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+set -f # disable globbing
+export IFS=' '
+
+export S3_BUCKET=${S3_BUCKET:-dividat-ci-nix-cache}
+export S3_BUCKET_PARAMS=${S3_BUCKET_PARAMS:-compression=zstd&profile=nixcache}
+
+# how many uploads to perform in parallel
+export TS_SLOTS=10
+
+echo "Queuing cache upload of: $OUT_PATHS"
+#tsp nix copy --to "s3://${S3_BUCKET}?${S3_BUCKET_PARAMS}" $OUT_PATHS
+# noop for now
+# TODO: how to prevent uploads of large packages?
+tsp echo $OUT_PATHS
