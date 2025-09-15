c7a89eb1968dabe690e65ff30a4ec325c0dc1333
Ignas Vyšniauskas <ignas@dividat.ch>
2025-09-15 16:53:49 +0300

    Fix kiosk tests and clean-up dev environment setup
    The exec in shellHook introduced with ae1568c13a3a5e43 (nearly two years
    ago!) prevents `nix-shell --run` from executing, therefore our CI has
    not been running kiosk tests for... a long time now.
    
    This commit fixes the situation by simply sourcing the required Qt env
    vars instead of exec`ing a custom shell.
    
    Additionally, test dependencies and setup is separated from the build
    inputs.

--- kiosk/shell.nix
+++ kiosk/shell.nix
@@ -1,10 +1,27 @@
 let
   pkgs = import ../pkgs {};
-in
-  import ./default.nix {
+  kiosk = import ./default.nix {
     pkgs = pkgs;
     system_name = "PlayOS";
     system_version = "1.0.0-dev";
-    # Provides qtwayland only for testing
-    additional_inputs = [ pkgs.qt6.qtwayland ];
+    # enable test deps
+    doCheck = true;
+  };
+in
+  pkgs.mkShell {
+    inputsFrom = [ kiosk ];
+
+    shellHook = ''
+      # Give access to kiosk_browser module
+      export PYTHONPATH=./:$PYTHONPATH
+
+      export FOCUS_SHIFT_PATH="${pkgs.focus-shift.main}"
+
+      # Setup Qt environment.. in a hacky way
+      tmpdir=$(mktemp -d)
+      makeWrapper /bin/true "$tmpdir/setupQtEnv" "''${qtWrapperArgs[@]}"
+      # remove the final exec
+      sed -i '\|exec "/bin/true"|d' "$tmpdir/setupQtEnv"
+      source "$tmpdir/setupQtEnv"
+    '';
   }
