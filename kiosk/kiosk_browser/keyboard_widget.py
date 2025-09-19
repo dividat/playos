fe5a147287d46e9f6183352288964b32916bdcb3
Ignas Vyšniauskas <ignas@dividat.ch>
2025-09-19 11:32:34 +0300

    Fix: ensure signal slots are tied to QObject instances
    Using lambdas or inline def's not tied to a QObject means that Qt
    will not know when to disconenct a signal slot. This can lead to
    situations where the connect handler is called, but the parent object
    holding the lambda no longer exists, leading to segfault.

--- kiosk/kiosk_browser/keyboard_widget.py
+++ kiosk/kiosk_browser/keyboard_widget.py
@@ -52,13 +52,13 @@ class ActivationKeyFilter(QtCore.QObject):
 
         self._supress_next_key_release: Optional[QtCore.Qt.Key] = None
 
-        def handle_focus_object_changed(old, new):
-            if old:
-                old.removeEventFilter(self)
-            if new:
-                new.installEventFilter(self)
+        focus_object_tracker.focusObjectChanged.connect(self._handle_focus_object_changed)
 
-        focus_object_tracker.focusObjectChanged.connect(handle_focus_object_changed)
+    def _handle_focus_object_changed(self, old, new):
+        if old:
+            old.removeEventFilter(self)
+        if new:
+            new.installEventFilter(self)
 
     # Note: installed for the top-level QApplication instance, so this is
     # performance-sensitive.
