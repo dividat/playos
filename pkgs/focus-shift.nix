2ccc437569be30cc343fa96aea23a00c5c9d3543
Ignas Vyšniauskas <ignas@dividat.ch>
2025-10-27 16:47:31 +0200

    Update focus-shift to version with focus-shift:exhausted events

--- pkgs/focus-shift.nix
+++ pkgs/focus-shift.nix
@@ -1,11 +1,11 @@
 { pkgs }:
 let
-    rev = "1e43419e1562dbe10ab4ce2a768e0fd0d51148bb";
+    rev = "d02a1db4b43eabef292dfe2139999fa3d08fca09";
 in
 {
     version = rev;
     main = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/dividat/focus-shift/${rev}/index.js";
-        hash = "sha256-dOGP2y5YV/+nB25hpBj04CL8yjYH7CIEg+QjfAirzDI=";
+        hash = "sha256-6IE5EiGioogRhRKB1WGslYP9uPiWZxk+Gctmc4hmCLs=";
     };
 }
