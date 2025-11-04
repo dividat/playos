180909f790e572da60090a6f4f95bc66f4b2a88a
Ignas Vyšniauskas <ignas@dividat.ch>
2025-11-04 10:13:01 +0200

    Bump focus-shift to latest main

--- pkgs/focus-shift.nix
+++ pkgs/focus-shift.nix
@@ -1,11 +1,11 @@
 { pkgs }:
 let
-    rev = "d02a1db4b43eabef292dfe2139999fa3d08fca09";
+    rev = "b475f7bb38ad0f7fbe157878b32b28273b55522e";
 in
 {
     version = rev;
     main = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/dividat/focus-shift/${rev}/index.js";
-        hash = "sha256-6IE5EiGioogRhRKB1WGslYP9uPiWZxk+Gctmc4hmCLs=";
+        hash = "sha256-WFeJHZBmyglYuazb574XNi5lFmwqjSsMHlYeBhzpom4=";
     };
 }
