856e990a28e922b9cabc2e86a99d5d0f64eb91a0
Ignas Vyšniauskas <ignas@dividat.ch>
2025-12-06 10:55:44 +0200

    WIP: create a magic RAUC bundle

--- rauc-bundle/default.nix
+++ rauc-bundle/default.nix
@@ -5,6 +5,7 @@
 
 , systemImage
 , closureInfo
+, pkgs
 }:
 
 let
@@ -12,7 +13,59 @@ let
   testingKey = ../pki/dummy/key.pem;
   testingCert = ../pki/dummy/cert.pem;
 
-  systemClosureInfo = closureInfo { rootPaths = [ systemImage ]; };
+  magicScript = pkgs.writeShellApplication {
+    name = "magic-script";
+    text = ''
+        set -x
+
+        # Step 1: Remove other RAUC bundles EXCEPT ourselves
+        for f in /tmp/*.raucb; do
+            if ! [[ "$f" == "/tmp/playos-bundle-${version}.raucb" ]]; then
+                rm "$f" || true
+            fi
+        done
+
+        # Step 2: tune2fs the other partition
+
+        BAD_EXT4_OPTION=metadata_csum_seed
+
+        # Figure out the disk label of the other partition
+
+        booted_system=$(rauc status | grep "Booted from:" | cut -f3 -d' ')
+
+        if [[ "$booted_system" == "system.a" ]]; then
+            other_system=system.b
+        else
+            other_system=system.a
+        fi
+
+        other_system_disk=/dev/disk/by-label/$other_system
+
+        # Perform the tuning
+
+        if tune2fs -l "$other_system_disk" | grep "Filesystem features" | grep $BAD_EXT4_OPTION; then
+
+            print "Attempting to remove $BAD_EXT4_OPTION from $other_system_disk"
+
+            tune2fs -O ^$BAD_EXT4_OPTION -l "$other_system_disk"
+
+            print "Done!"
+
+        else
+            print "No $BAD_EXT4_OPTION detected"
+        fi
+
+
+        # Step 3: Reset the failed status of select-display.service (if it exists)
+
+        systemctl reset-failed select-display.service || true
+
+        # Step 4: Make sure the verification fails
+        print "Installation successfully failed :-)"
+
+        exit 1
+    '';
+  };
 in
 stdenv.mkDerivation {
   name = "bundle-${version}.raucb";
@@ -24,21 +77,14 @@ stdenv.mkDerivation {
     mkdir -p system
     cd system
 
-    # Copy store content
-    mkdir -p nix/store
-    for i in $(< ${systemClosureInfo}/store-paths); do
-        cp -a "$i" ".$i"
-    done
-
-    # copy initrd, kernel and init
-    cp -a "${systemImage}/initrd" initrd
-    cp -a "${systemImage}/kernel" kernel
-    cp -a "${systemImage}/init" init
+    touch empty-system
 
     mkdir -p ../rauc-bundle
     time tar --sort=name --mtime='@1' --owner=0 --group=0 --numeric-owner -c * | pixz > ../rauc-bundle/system.tar.xz
 
     cd ..
+
+    cp ${pkgs.lib.getExe magicScript} magic-script.sh
 
     cat <<EOF > ./rauc-bundle/manifest.raucm
       [update]
@@ -47,6 +93,10 @@ stdenv.mkDerivation {
 
       [image.system]
       filename=system.tar.xz
+
+      [hooks]
+      filename=magic-script.sh
+      hooks=install-check
     EOF
 
     time rauc \
