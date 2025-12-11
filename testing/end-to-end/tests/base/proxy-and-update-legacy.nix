f9a0bb9d736c028f1c2cfb6b9af5b968abd57ad8
Ignas Vyšniauskas <ignas@dividat.ch>
2025-12-11 11:29:52 +0200

    Specify which versions are problematic and 'legacy'

--- testing/end-to-end/tests/base/proxy-and-update-legacy.nix
+++ testing/end-to-end/tests/base/proxy-and-update-legacy.nix
@@ -1,5 +1,5 @@
 # Runs the same test as proxy-and-update, but in "legacy" mode that assumes
-# older PlayOS systems, where mke2fs has not been configured to exclude
-# unsupported ext4 features.
+# problematic older PlayOS systems (2025.3.{0,1,2}), where mke2fs has not been
+# configured to exclude unsupported ext4 features.
 args@{pkgs, ...}:
 pkgs.callPackage ./proxy-and-update.nix (args // { legacyMode = true; })
