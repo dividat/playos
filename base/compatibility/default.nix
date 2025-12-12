7386695f1c0f9c5a9663b1cb7134c58ba84db291
Ignas Vyšniauskas <ignas@dividat.ch>
2025-12-12 09:21:12 +0200

    Switch definition order and modify comment

--- base/compatibility/default.nix
+++ base/compatibility/default.nix
@@ -2,9 +2,19 @@
 {
     config = {
         # Disable ext4 features that are not supported by older GRUB versions,
-        # used by in PlayOS installations up to and including version 2023.2.0.
+        # used by in PlayOS installations up to and including version 2023.2.0
+        # by providing a global mke2fs.conf.
+        #
         # See: https://bugs.launchpad.net/ubuntu/+source/grub2/+bug/1844012
         # Debian patch: https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git/commit/?h=debian/master&id=1181f164c48574a4813bfa203dbd7b4123154021
+        environment.etc."mke2fs.conf" = {
+            # e2fsprogs v1.47.3 mke2fs.conf with two modifications:
+            # metadata_csum_seed and orphan_file are set to disabled in ext4
+            # source: https://github.com/tytso/e2fsprogs/blob/v1.47.3/misc/mke2fs.conf.in
+            source = ./mke2fs.conf;
+        };
+
+        # Ensure mke2fs is used in login/shell sessions
         environment.sessionVariables.MKE2FS_CONFIG = "/etc/mke2fs.conf";
 
         # Ensure any process started by systemd has this in the config, in
@@ -14,11 +24,5 @@
             DefaultEnvironment="MKE2FS_CONFIG=/etc/mke2fs.conf"
         '';
 
-        environment.etc."mke2fs.conf" = {
-            # Default e2fsprogs v1.47.3 mke2fs.conf with two modifications:
-            # metadata_csum_seed and orphan_file are set to disabled in ext4
-            # source: https://github.com/tytso/e2fsprogs/blob/v1.47.3/misc/mke2fs.conf.in
-            source = ./mke2fs.conf;
-        };
     };
 }
