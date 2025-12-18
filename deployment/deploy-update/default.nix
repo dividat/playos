b4475b4b0fa500dda4afdb2698363079960f4139
Joris Guyonvarch <guyonvarch@dividat.com>
2025-12-18 16:07:23 +0100

    Use sops for signing rauc when deploying

--- deployment/deploy-update/default.nix
+++ deployment/deploy-update/default.nix
@@ -1,12 +1,12 @@
 { substituteAll
 , application, updateCert, unsignedRaucBundle, docs, installer, live
 , deployUrl, updateUrl, kioskUrl
-, rauc, awscli, python3
+, rauc, awscli, python3, sops
 }:
 substituteAll {
   src = ./deploy-update.py;
   dummyBuildCert = ../../pki/dummy/cert.pem;
   inherit updateCert unsignedRaucBundle docs installer live deployUrl updateUrl kioskUrl;
-  inherit rauc awscli python3;
+  inherit rauc awscli python3 sops;
   inherit (application) fullProductName safeProductName version;
 }
