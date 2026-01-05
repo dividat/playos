6587fa0766dfd38e145ec54b55d0a16e3a400033
Johannes Emerich <johannes@dividat.com>
2026-01-05 11:47:50 +0100

    Bump ppx_protocol_conv for compatibility with ppxlib in nixpkgs 25.11

--- pkgs/ocaml-modules/ppx_protocol_conv/default.nix
+++ pkgs/ocaml-modules/ppx_protocol_conv/default.nix
@@ -11,15 +11,15 @@
 
 buildDunePackage rec {
   pname = "ppx_protocol_conv";
-  version = "5.2.1";
+  version = "5.2.3";
 
   minimumOCamlVersion = "4.08";
 
   src = fetchFromGitHub {
     owner = "andersfugmann";
     repo = "ppx_protocol_conv";
-    rev = "0a4fd698932f20345cb3ba12bab82a9eda4d9147";
-    sha256 = "sha256-gFYg0251NOPPkc01BiSU3NEj3JWGKg1fu6UgQ67BBZM=";
+    rev = "425a7a1a26c26fcb740e5574e252043fd03eff46";
+    sha256 = "sha256-G6zMgHioPUCh2i50cjQ6talN8CYO6UhEaWgi0xe3CWA=";
   };
 
   useDune2 = true;
