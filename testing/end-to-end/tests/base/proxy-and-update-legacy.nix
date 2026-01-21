8133af9008c39cf90172241ecfcedc84a2f80e45
Ignas Vyšniauskas <ignas@dividat.ch>
2026-01-21 15:39:41 +0200

    Remove the post-install compatibility hook and tests
    For the future reader: we assume that these compat hooks are no longer
    needed. If they are, they should be incorporated in the install-check
    and/or other hook stages. See the next commit, which re-adds a generic
    compat install-check script and hook.

--- testing/end-to-end/tests/base/proxy-and-update-legacy.nix
+++
@@ -1,5 +0,0 @@
-# Runs the same test as proxy-and-update, but in "legacy" mode that assumes
-# problematic older PlayOS systems (2025.3.{0,1,2}), where mke2fs has not been
-# configured to exclude unsupported ext4 features.
-args@{pkgs, ...}:
-pkgs.callPackage ./proxy-and-update.nix (args // { legacyMode = true; })
