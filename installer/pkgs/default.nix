dc92081eb0a81873503c5d51bca12b09376a48ee
Ignas Vyšniauskas <ignas@dividat.ch>
2026-03-05 16:34:29 +0200

    TEST: downgrade installer nixpkgs to previous release
    Forcing divergence between system and installer / skeleton to check
    whether e2e tests pass and etc.

--- installer/pkgs/default.nix
+++ installer/pkgs/default.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = builtins.fetchTarball {
-    # release-24.11 2025-02-10
-    url = "https://github.com/NixOS/nixpkgs/archive/edd84e9bffdf1c0ceba05c0d868356f28a1eb7de.tar.gz";
-    sha256 = "1gb61gahkq74hqiw8kbr9j0qwf2wlwnsvhb7z68zhm8wa27grqr0";
+    # nixos-24.05 2024-10-07
+    url = "https://github.com/nixos/nixpkgs/archive/ecbc1ca8ffd6aea8372ad16be9ebbb39889e55b6.tar.gz";
+    sha256 = "0yfaybsa30zx4bm900hgn3hz92javlf4d47ahdaxj9fai00ddc1x";
   };
 
   overlay =
