52653ffbc5adf5a29313d4f9e151dfbe92ece66e
Ignas Vyšniauskas <ignas@dividat.ch>
2026-04-17 14:13:11 +0300

    Add a custom installer greeting ASCII art
    It is a... a kind of a hedgehog?

--- skeleton/configuration.nix
+++ skeleton/configuration.nix
@@ -9,17 +9,19 @@ let
     # We version the installer according to the version of PlayOS it installs!
     version = systemMetadata.version;
 
-    # TODO: WIP ^_^
     greeting = label: ''
-             ░░░░
-           ░▒▒▒▒▒▒▒  ░    ▒
-        ▒▒▒▒▒   ░░▒▒░░░▒▒▒▒▒ ▒▒
-      ▒▒          ░░░░░░░   ▒▒▒▒▒
-     ▒▒▒      ▲           ▲     ▒▒
-     ▒░░                         ░
-       ░░░                       ░
-         ░░░▒▒      ▒▒░░░  ▒▒▒   ░░ ${label}
-             ▒▒▒▒▒▒▒▒    ░░░▒▒▒░░░
+                 ,¸,¸¸,¸,
+               ¸,░░░░░░░░´,
+             ,░░░░░░░░░░░░░´¸
+            ⌠░░░░░░░░░░░░░░░░´
+           ⌠  ░░░░░░░░░░░░░░░░`
+          /``  ░░░░░░░░░░░░░░░░`
+         /      ░░░░░░░░░░░░░░'
+        °¯¯¯¯¯¯(¨(¨¨¨¨¨¨(¨(¨¨¨
+     ☼         ´ ´      ´ ´
+      ☼
+       ${label}
+       ${strings.stringAsChars (char: "≈") label}
     '';
 
 in
